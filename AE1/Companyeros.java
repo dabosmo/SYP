886ee0c77999681f58af46519e484bb10a732eca
= <=>
2020-10-06 16:58:18 +0200

    AE1_2a Ejercicio 2 con vector

---
+++ AE1/Companyeros.java
@@ -0,0 +1,16 @@
+package AE1;
+
+public class Companyeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] comps = new String[] {"Daniel", "Victor", "Alejandra", "Laura", "Javier", "Carmen"};
+		
+		for(int i = 0; i < 6; i++) {
+			
+			System.out.println(comps[i]);
+		}
+
+	}
+
+}
