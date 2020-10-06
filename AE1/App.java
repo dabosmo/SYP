a7913d05f67bc01505879cb0f05103af1983ea40
= <=>
2020-10-06 16:48:10 +0200

    AE1_1 tarea finalizada

---
+++ AE1/App.java
@@ -0,0 +1,16 @@
+package AE1;
+
+public class App {
+	
+	public static void main (String args[]) {
+		
+		System.out.println(sayHello());
+	}
+	
+	public static String sayHello(){
+		
+		String mensaje = "Hola Mundo";
+		
+		return mensaje;
+	}
+}
